55c5bf526cb362f60fd98449ec39ee564db1b589
cx-shubham-singhal <204662522+cx-shubham-singhal@users.noreply.github.com>
2025-11-04 15:22:49 +0530

    ReportChanges

--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -3,6 +3,7 @@ package utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.myorg.cxone.helpers.EnvValidator;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
@@ -18,23 +19,31 @@ public class Base {
         extent = ReportManager.getInstance();
     }
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true)
     public void setupClass() {
         String className = this.getClass().getSimpleName();
         classLevelTest.set(extent.createTest(className));
+        System.out.println("\n=== Starting Test Class: " + className + " ===");
     }
 
-    @BeforeMethod
-    public void setupTest(Method method) {
+    @BeforeMethod(alwaysRun = true)
+    public void setupTest(Method method, ITestResult result) {
         String testName = method.getName();
+
+        // Create node under class-level test
         ExtentTest test = classLevelTest.get().createNode(testName);
         testLevelTest.set(test);
+
+        // Log to console + Extent report
+        System.out.println(">>> Starting Test: " + testName);
+        test.info("Starting Test: " + testName);
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDownSuite() {
         if (extent != null) {
             extent.flush();
+            System.out.println("\n✅ Extent report flushed successfully after suite execution.");
         }
     }
 
